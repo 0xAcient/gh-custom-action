622c18e443a6eefbe51b2056d8bc4671ac0a27f2
0xAcient <acientguru@gmail.com>
2026-01-17 08:50:19 +0100

    chores added a s3 deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,7 +4,16 @@ const exec = require('@actions/exec');
 
 
 function run() {
-    core.notice("Hello from my custom javascript actions")
+
+    //get  some input value
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+    
+
+    //Upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 }
 
 
